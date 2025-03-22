c3562180c0064ac391cc89bccd0bc252f5072703
martin-yovchev <martin.yovchev@sentecacommerce.com>
2025-03-22 16:59:27 +0200

    Customer changes

---
+++ tarnslations.js
@@ -0,0 +1,140 @@
+const translations = {
+  bg: {
+    translation: "BG",
+    menu: "Меню",
+    home: "Начало",
+    about: "За нас",
+    activities: "Дейности",
+    goals: "Цели",
+    contact: "Контакти",
+    title: "Център за Устойчиви Политики",
+    mobileTitle: "Устойчиви Политики",
+    subtitle:
+      "Сдружение с нестопанска цел регистрирано на 1.11.2024 г. като ЮЛНЦ в обществена полза",
+    contactUs: "Свържете се с нас",
+    learnMore: "Научете повече",
+    aboutText:
+      "Сдружението е учредено от 7 физически лица за постигане на широк кръг от цели, фокусирани върху устойчиво развитие и иновации.",
+    ourGoals: "Нашите цели",
+    ourGoalsText:
+      "Развитие на духовни ценности, гражданско общество и защита на човешките права. Насърчаване на взаимодействието между НПО-тата, държавата и бизнеса.",
+    ourActivities: "Нашите дейности",
+    ourActivitiesText:
+      "Развитие на проекти, консултантски услуги, обучение и събития. Сътрудничество с организации в България и чужбина.",
+    ourTeam: "Нашият екип",
+    ourTeamText:
+      "Екипът на сдружението притежава богат професионален опит в областите:",
+    mainGoals: "Основните ни цели",
+    focus: "Фокус върху устойчиво развитие, иновации и социална отговорност",
+    address: "гр. Враца, ул. Христо Ботев № 115, вх. Е, ап. 85",
+    values: "Развитие на ценности",
+    valuesText:
+      "Развитие и утвърждаване на духовни ценности, гражданско общество, защита на човешките права.",
+    cooperation: "Сътрудничество",
+    cooperationText:
+      "Стимулиране на сътрудничеството между неправителствени организации, държавни институции и частния сектор.",
+    innovation: "Иновации",
+    innovationText:
+      "Насърчаване на научно-изследователската дейност и технологично развитие.",
+    education: "Образование",
+    educationText:
+      "Насърчаване на инвестициите в наука и образование, обучение и подготовка на кадри за бизнеса.",
+    entrepreneurship: "Предприемачество",
+    entrepreneurshipText:
+      "Подпомагане на малки и средни предприятия и предприемачество.",
+    sustainability: "Устойчивост",
+    sustainabilityText:
+      "Насърчаване на ресурсната ефективност и зеления растеж.",
+    copyright: "© Център за Устойчиви Политики",
+    innovationLabel: "Инновации",
+    sustainabilityLabel: "Устойчивост",
+    developmentLabel: "Развитие",
+    twitterLabel: "Twitter",
+    facebookLabel: "Facebook",
+    instagramLabel: "Instagram",
+    linkedinLabel: "LinkedIn",
+    emailLabel: "Имейл",
+    aboutHeading: "За нас",
+    contactOverlayTitle: "Свържете се с нас",
+    environment: "Околна среда",
+    environmentText:
+      "Опазване на околната среда и биоразнообразието, насърчаване на действията за адаптация към изменението на климата.",
+    crossBorderCooperation: "Трансгранично сътрудничество",
+    crossBorderCooperationText:
+      "Насърчаване на устойчивото развитие на регионите и на трансграничното сътрудничество.",
+    servicesAnalysis:
+      "Изготвяне на анализи и проучвания в областта на разработване и внедряване на иновации в МСП, подпомагане на предприемаческото обучение, трансфера на знания и др.",
+    servicesConsulting:
+      "Предоставяне на техническо, консултантско и експертно съдействие на лица, които имат нужда от съдействие за осигуряване на достъп до финансиране, оказване на помощ при подготовка на документи, изисквани при кандидатстване за финансиране по проекти.",
+    servicesTraining: "Организиране и провеждане на обучения.",
+  },
+  en: {
+    translation: "EN",
+    menu: "Menu",
+    home: "Home",
+    about: "About",
+    activities: "Activities",
+    goals: "Goals",
+    contact: "Contact",
+    title: "Center for Sustainable Policies",
+    mobileTitle: "Sustainable Policies",
+    subtitle:
+      "Non-profit organization registered on 1.11.2024 as a legal entity in public benefit",
+    contactUs: "Contact Us",
+    learnMore: "Learn More",
+    aboutText:
+      "The organization was founded by 7 individuals to achieve a wide range of goals focused on sustainable development and innovation.",
+    ourGoals: "Our Goals",
+    ourGoalsText:
+      "Development of spiritual values, civil society, and human rights protection. Encouraging interaction between NGOs, government, and business.",
+    ourActivities: "Our Activities",
+    ourActivitiesText:
+      "Development of projects, consulting services, training, and events. Cooperation with organizations in Bulgaria and abroad.",
+    ourTeam: "Our Team",
+    ourTeamText:
+      "The association's team has extensive professional experience in the following areas:",
+    mainGoals: "Our Main Goals",
+    focus:
+      "Focus on sustainable development, innovation, and social responsibility",
+    address: "115 Hristo Botev St., Entrance E, Apt. 85, Vratsa",
+    values: "Values Development",
+    valuesText:
+      "Development and establishment of spiritual values, civil society, human rights protection.",
+    cooperation: "Cooperation",
+    cooperationText:
+      "Encouraging cooperation between non-governmental organizations, government institutions, and the private sector.",
+    innovation: "Innovation",
+    innovationText:
+      "Encouraging research activities and technological development.",
+    education: "Education",
+    educationText:
+      "Encouraging investments in science and education, training and preparation of business personnel.",
+    entrepreneurship: "Entrepreneurship",
+    entrepreneurshipText:
+      "Supporting small and medium enterprises and entrepreneurship.",
+    sustainability: "Sustainability",
+    sustainabilityText: "Encouraging resource efficiency and green growth.",
+    copyright: "© Center for Sustainable Policies",
+    innovationLabel: "Innovation",
+    sustainabilityLabel: "Sustainability",
+    developmentLabel: "Development",
+    twitterLabel: "Twitter",
+    facebookLabel: "Facebook",
+    instagramLabel: "Instagram",
+    linkedinLabel: "LinkedIn",
+    emailLabel: "Email",
+    aboutHeading: "About Us",
+    contactOverlayTitle: "Contact Us",
+    environment: "Environment",
+    environmentText:
+      "Protection of the environment and biodiversity, promoting actions for climate change adaptation.",
+    crossBorderCooperation: "Cross-Border Cooperation",
+    crossBorderCooperationText:
+      "Promoting the sustainable development of regions and cross-border cooperation.",
+    servicesAnalysis:
+      "Conducting analyses and research in the field of innovation development and implementation in SMEs, supporting entrepreneurial training, knowledge transfer, and more.",
+    servicesConsulting:
+      "Providing technical, consulting, and expert assistance to individuals in need of support for access to funding, helping with the preparation of documents required for project funding applications.",
+    servicesTraining: "Organizing and conducting training sessions.",
+  },
+};
